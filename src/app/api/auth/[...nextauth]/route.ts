971d8ca7c30b2dc6bf52d45c009109215db7e892
Atharva <atharvaraj@gmail.com>
2025-04-18 16:19:33 +0530

    auth + db + shad

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
